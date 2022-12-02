66aa690a14105ab1355f604d2b7c60827d4e8a44
ViniciusDiefenbach <viniciusdiefenbach2004@gmail.com>
2022-12-01 23:35:57 -0300

    Upload de imagens

---
+++ UploadImages/custom.js
@@ -0,0 +1,5 @@
+// Local source
+document.getElementById("local-source").innerHTML = "<img src='images/image.png' width='250px'>"
+
+// Remote source
+document.getElementById("remote-source").innerHTML = "<img src='https://c4.wallpaperflare.com/wallpaper/64/393/135/razer-hd-wallpaper-preview.jpg' width='250px'>"
